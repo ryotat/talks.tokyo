25bf1586e82ccb178ae5134fff81f630e3d06326
Ryota Tomioka <ryotat@new-host.fletsphone>
2012-08-19 14:08:48 +0900

    view/talk/edit.rhmlt - Implemented a modal dialog. smartform.js - Can handle american style date correctly. - Can handle "Location" and "Lecturer" talks-screen.css - styles for #smartform and #lean_overlay

--- public/javascripts/smartform.js
+++ public/javascripts/smartform.js
@@ -1,5 +1,3 @@
-var del = "：:\\s"; // delimiters
-
 // String型にreplaceAll()関数を追加
 String.prototype.replaceAll = function ( before, after ) {
     return this.split( before ).join( after );  
@@ -9,78 +7,83 @@ String.prototype.strip = function() {
     return String(this).replace(/^\s+|\s+$/g, '');
 }
 
-var Item = function(id, key, re, joiner) {
-    this.id    = id;
-    this.hasDefaultValue=true;
-    this.strArray   = new Array();
-    if (re) {
-	this.re    = new RegExp("^(?:"+key+")\\s*["+del+"]\\s*(?:"+re+")|^(?:"+key+")$");
-    }
-    if (joiner) {
-	this.joiner = joiner;
-    }
-    else {
-	this.joiner = "";
-    }
-}
-
-Item.prototype = {
-    set_default: function(item) {
-	if (this.hasDefaultValue && item.strArray.length>0) {
-	    this.strArray = item.strArray;
-	}
-    },
-    parse_line: function(line) {
-	var match = line.match(this.re);
-	if (match && match.length>=2) {
-	    if (this.strArray.length>0) {
-		if (this.hasDefaultValue) { // Existing entry is a default value, overwrite
-		    this.insert_line(match[1]);
-		}
-		else { // Signal the beginning of a new talk by new Item
-		    var tmp=new Item(this.id);
-		    tmp.re = this.re;
-		    tmp.joiner = this.joiner;
-		    tmp.hasDefaultValue=true;
-		    tmp.insert_line(match[1]);
-		    tmp.show = this.show;
-		    return tmp;
-		}
-	    }
-	    else {
-		this.insert_line(match[1]);
-	    }
-	    return true;
-	}
-	return false;
-    },
-    insert_line: function(line) {
-	if (line && line.length>0) { // Insert no empty line
-	    if (this.hasDefaultValue) { // Overwrite default value
-		this.strArray = [line];
-		this.hasDefaultValue=false;
-	    }
-	    else { // Insert line at the end
-		this.strArray.push(line);
-	    }
-	}
-    },
-    show: function(index) {
-	if (this.strArray.length>0) {
-	    this.show_str(index,this.strArray.join(this.joiner));
-	}
-    },
-    show_str: function(index,str) {
-	$("#"+this.id+index).val(str.strip());
-    }
-};
-
 function append_empty_form(sel, i) {
-	$(sel).append('<form action="http://localhost:3000/talk/update" enctype="multipart/form-data" id="edittalk" method="post"><input id="talk_series_id" name="talk[series_id]" type="hidden" value="5" /><dt>Date YYYY/MM/DD    Time HH:MM</dt><dd><input id="talk_date_string'+i+'" name="talk[date_string]" size="11" type="text" /> From <input id="talk_start_time_string'+i+'" name="talk[start_time_string]" size="5" type="text" /> to <input id="talk_end_time_string'+i+'" name="talk[end_time_string]" size="5" type="text" /></dd><dt>Title</dt><dd><input class="wide" id="talk_title'+i+'" name="talk[title]" size="60" type="text" /></dd><dt>Abstract</dt><dd><textarea class="wide" cols="57" id="talk_abstract'+i+'" name="talk[abstract]" rows="10"></textarea></dd><dt>Speaker\'s name and affiliation</dt><dd><input class="wide" id="talk_name_of_speaker'+i+'" name="talk[name_of_speaker]" size="60" type="text" /></dd><dt>Speaker\'s e-mail</dt><dd><input class="wide" id="talk_speaker_email" name="talk[speaker_email]" size="60" type="text" value="" /><p class="emailcheck"><input id="talk_send_speaker_email" name="talk[send_speaker_email]" type="checkbox" value="1" /><input name="talk[send_speaker_email]" type="hidden" value="0" />Check this box to send an e-mail to the speaker when you save this talk.</p></dd><dt>Venue</dt><dd><input class="wide" id="talk_venue_name'+i+'" name="talk[venue_name]" size="60" type="text" value="Venue to be confirmed" /></dd><p><input name="commit" type="submit" value="Save" /> or <a href="http://localhost:3000/show/index/7">Cancel</a></p></form>').hide().fadeIn("fast");
+	jQuery(sel).append('<form action="http://localhost:3000/talk/update" enctype="multipart/form-data" id="edittalk" method="post"><input id="talk_series_id" name="talk[series_id]" type="hidden" value="5" /><dt>Date YYYY/MM/DD    Time HH:MM</dt><dd><input id="talk_date_string'+i+'" name="talk[date_string]" size="11" type="text" /> From <input id="talk_start_time_string'+i+'" name="talk[start_time_string]" size="5" type="text" /> to <input id="talk_end_time_string'+i+'" name="talk[end_time_string]" size="5" type="text" /></dd><dt>Title</dt><dd><input class="wide" id="talk_title'+i+'" name="talk[title]" size="60" type="text" /></dd><dt>Abstract</dt><dd><textarea class="wide" cols="57" id="talk_abstract'+i+'" name="talk[abstract]" rows="10"></textarea></dd><dt>Speaker\'s name and affiliation</dt><dd><input class="wide" id="talk_name_of_speaker'+i+'" name="talk[name_of_speaker]" size="60" type="text" /></dd><dt>Speaker\'s e-mail</dt><dd><input class="wide" id="talk_speaker_email" name="talk[speaker_email]" size="60" type="text" value="" /><p class="emailcheck"><input id="talk_send_speaker_email" name="talk[send_speaker_email]" type="checkbox" value="1" /><input name="talk[send_speaker_email]" type="hidden" value="0" />Check this box to send an e-mail to the speaker when you save this talk.</p></dd><dt>Venue</dt><dd><input class="wide" id="talk_venue_name'+i+'" name="talk[venue_name]" size="60" type="text" value="Venue to be confirmed" /></dd><p><input name="commit" type="submit" value="Save" /> or <a href="http://localhost:3000/show/index/7">Cancel</a></p></form>').hide().fadeIn("fast");
 
 }
 
 function parse_smart_form(box) {
+    var del = "：:\\s"; // delimiters
+
+    // Define Item
+    var Item = function(id, key, re, joiner) {
+	this.id    = id;
+	this.hasDefaultValue=true;
+	this.strArray   = new Array();
+	if (re) {
+	    this.re    = new RegExp("^(?:"+key+")\\s*["+del+"]\\s*(?:"+re+")|^(?:"+key+")$");
+	}
+	if (joiner) {
+	    this.joiner = joiner;
+	}
+	else {
+	    this.joiner = "";
+	}
+    }
+
+    Item.prototype = {
+	set_default: function(item) {
+	    if (this.hasDefaultValue && item.strArray.length>0) {
+		this.strArray = item.strArray;
+	    }
+	},
+	parse_line: function(line) {
+	    var match = line.match(this.re);
+	    if (match && match.length>=2) {
+		if (this.strArray.length>0) {
+		    if (this.hasDefaultValue) { // Existing entry is a default value, overwrite
+			this.insert_line(match[1]);
+		    }
+		    else { // Signal the beginning of a new talk by new Item
+			var tmp=new Item(this.id);
+			tmp.re = this.re;
+			tmp.joiner = this.joiner;
+			tmp.hasDefaultValue=true;
+			tmp.insert_line(match[1]);
+			tmp.show = this.show;
+			return tmp;
+		    }
+		}
+		else {
+		    this.insert_line(match[1]);
+		}
+		return true;
+	    }
+	    return false;
+	},
+	insert_line: function(line) {
+	    if (line && line.length>0) { // Insert no empty line
+		if (this.hasDefaultValue) { // Overwrite default value
+		    this.strArray = [line];
+		    this.hasDefaultValue=false;
+		}
+		else { // Insert line at the end
+		    this.strArray.push(line);
+		}
+	    }
+	},
+	show: function(index) {
+	    if (this.strArray.length>0) {
+		this.show_str(index,this.strArray.join(this.joiner));
+	    }
+	},
+	show_str: function(index,str) {
+	    jQuery("#"+this.id+index).val(str.strip());
+	    jQuery("#"+this.id+index).removeClass('blur');
+	}
+    };
+
+    // Main
     (function(box, Talk) {
     var talk = new Talk();
 
@@ -112,11 +115,11 @@ function parse_smart_form(box) {
     }
     talks.push(talk);
 
-    $('#box').empty()
+    // $('#box').empty()
     for (var i=0; i<talks.length; i++) {
-	append_empty_form('#box',i);
+	// append_empty_form('#box',i);
 	for (var key in talks[i]) {
-	    talks[i][key].show(i);
+	    talks[i][key].show('');
 	}
     }
     })(box,  // Define Talk Object here
@@ -126,13 +129,13 @@ function parse_smart_form(box) {
 			    "Time|Date\\s*(?:& Time|)|日時|日程",
 			    "(.*)", "");
 	this.venue= new Item("talk_venue_name",
-			     "Place|Venue|@|場所|会場",
+			     "Place|Venue|Location|@|場所|会場",
 			     "(.*)",", ");
 	this.abst= new Item("talk_abstract",
 			    "Abstract|(?:講演|セミナー|)(?:アブストラクト|概要|要旨)(?:.*[Aa]bstract[^"+del+"]|)",
 			    "(.*)");
 	this.speaker= new Item("talk_name_of_speaker",
-			       "Speaker|(?:発表者|講演者|スピーカー|講師)(?:.*[Ss]peaker[^"+del+"]|)",
+			       "Speaker|Lecturer|(?:発表者|講演者|スピーカー|講師)(?:.*[Ss]peaker[^"+del+"]|)",
 			       "(.*)",", ");
 	this.supervisor= new Item("talk_name_of_speaker",
 				  "指導教員","(.*)",", ");
@@ -142,21 +145,62 @@ function parse_smart_form(box) {
 	this.comment.show = function(index) {
 	    // Don't show comments.
 	}
-	
+
 	this.date.show = function(index) {
 	    (function(obj, index, normalizeNumber, hour24, yearWestern) {
 	    var str = obj.strArray.join("");
-	    var re = new RegExp("(?:((?:平成\\s*|)[０-９\\d]+)\\s*[年/／]\\s*|)([０-９\\d]+)\\s*[月/／]\\s*([０-９\\d]+)\\s*(?:日|)(?:[^０-９\\d午前後]*\\s*((?:午前|午後|)\\s*[０-９\\d]+)\\s*[時:：](?:\\s*([０-９\\d]+)(?:分|)|)|)(?:\\s*(?:[-－ー〜～~]+|から)\\s*|)(?:((?:午前|午後|)[０-９\\d]+)\\s*[時:：](?:\\s*([０-９\\d]+)(?:分|)|)|)");
+	    var restr_time = "(?:[^０-９\\d午前後]*\\s*((?:午前|午後|)\\s*[０-９\\d]+)\\s*[時:：](?:\\s*([０-９\\d]+)(?:分|)|)|)(?:\\s*(?:[-－ー〜～~]+|から)\\s*|)(?:((?:午前|午後|)[０-９\\d]+)\\s*[時:：](?:\\s*([０-９\\d]+)(?:分|)|)|)";
+	    var re = new RegExp("(?:((?:平成\\s*|)[０-９\\d]+)\\s*[年/／]\\s*|)([０-９\\d]+)\\s*[月/／]\\s*([０-９\\d]+)\\s*(?:日|)"+restr_time);
 	    var match = str.match(re);
 	    if (match) {
 		var d=new Date();
 		var year=yearWestern(match[1] || d.getFullYear());
-		obj.show_str(index,normalizeNumber(year)+"/"
-			      +normalizeNumber(match[2])+"/"
-			      +normalizeNumber(match[3]));
-		$("#talk_start_time_string"+index).val(normalizeNumber(hour24(match[4]))+":"+normalizeNumber(match[5]||"00"));
-		$("#talk_end_time_string"+index).val(normalizeNumber(hour24(match[6]))+":"+normalizeNumber(match[7]||"00"));
+		var month=match[2];
+		var day=match[3];
 	    }
+	    else {
+		var re = new RegExp("(?:"
+				    +"(?:Monday|Mon)|"
+				    +"(?:Tuesday|Tue)|"
+				    +"(?:Wednesday|Wed)|"
+				    +"(?:Thursday|Thu)|"
+				    +"(?:Friday|Fri)|"
+				    +"(?:Saturday|Sat)|"
+				    +"(?:Sunday|Sun)|)[\\s,]*"
+				    +"((?:January|Jan)|"
+				    +"(?:February|Feb)|"
+				    +"(?:March|Mar)|"
+				    +"(?:April|Apr)|"
+				    +"May|"
+				    +"(?:June|Jun)|"
+				    +"(?:July|Jul)|"
+				    +"(?:August|Aug)|"
+				    +"(?:September|Sep)|"
+				    +"(?:October|Oct)|"
+				    +"(?:November|Nov)|"
+				    +"(?:December|Dec))\\s*"
+				    +"([０-９\\d]+)[sthrd\\s,]*"
+				    +"(?:([０-９\\d]+)|)"+restr_time);
+		var match=str.match(re);
+		if (match) {
+		    var d=new Date();
+		    var year=match[3] || d.getFullYear();
+		    var map = {"Jan":"1", "Feb":"2", "Mar":"3", "Apr":"4", "May":"5", "Jun":"6", "Jul":"7", "Aug":"8", "Sep":"9", "Oct":"10", "Nov":"11", "Dec":"12"};
+		    var month=map[match[1].substring(0,3)];
+		    var day=match[2];
+		}
+		else {
+		    return;
+		}
+	    }
+		obj.show_str(index,normalizeNumber(year)+"/"
+			     +normalizeNumber(month)+"/"
+			     +normalizeNumber(day));
+		jQuery("#talk_start_time_string"+index).val(normalizeNumber(hour24(match[4]))+":"+normalizeNumber(match[5]||"00"));
+		jQuery("#talk_end_time_string"+index).val(normalizeNumber(hour24(match[6]))+":"+normalizeNumber(match[7]||"00"));
+		
+		
+
 	    })(this, index, 
 	       // Define normalizeNumber
 	       function( inStr ){
@@ -227,7 +271,7 @@ function parse_smart_form(box) {
 	// 数理輪講のため
 	this.supervisor.show = function(index) {
 	    if (this.strArray.length>0) {
-		this.show_str(index, $("#"+this.id+index).val()+"　（指導教員："+this.strArray.join(this.joiner)+"）");
+		this.show_str(index, jQuery("#"+this.id+index).val()+"　（指導教員："+this.strArray.join(this.joiner)+"）");
 	    }
 	};
 
