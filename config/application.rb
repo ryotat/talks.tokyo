b155ad6d2f2a13ae66ad5287eea4d42d7b39d22b
Ryota Tomioka <ryotat@new-host.fletsphone>
2012-09-02 16:51:32 +0900

    Autoload lib/* Timezone is Tokyo (of course).

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module TalksTokyo
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -58,5 +59,10 @@ module TalksTokyo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Time zone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
   end
 end
