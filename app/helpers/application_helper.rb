2bbdd3117b45ed3aab674ac6444cab9c92fe4da5
Ryota Tomioka <ryotat@new-host-2.fletsphone>
2013-01-04 21:23:26 +0900

    Some more design fixes (including flash)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   end
   
   def show_flash
-    [:error, :warning, :confirm].map { |name| flash[name] ? "<div class=\"#{name}\">#{flash[name]}</div>" : "" }.join.html_safe
+    [:error, :warning, :confirm].map { |name| flash[name] ? "<div class=\"alert-#{name == :confirm ? "success" : "error"}\">#{flash[name]}</div>" : "" }.join.html_safe
   end
   
   def document(name, include_arrow = false, link_text = name )
