8f747d010681301a40fe8af6502a987397a078cc
Ryota Tomioka <ryotat@new-host-2.fletsphone>
2013-01-24 06:07:51 +0900

    Added test for sanity check.

--- spec/requests/custom_views_spec.rb
+++ spec/requests/custom_views_spec.rb
@@ -3,9 +3,18 @@ require 'spec_helper'
 describe "CustomViews" do
   describe "index" do
     let(:list) { FactoryGirl.create(:list) }
-    it "should not show text SITE_NAME" do
+    before do
+      add_random_talks(list)
       visit "/custom_view?list=#{list.id}"
+    end
+    it "should not show text SITE_NAME" do
       page.should_not have_content "SITE_NAME"
+    end
+    it "should generate a valid URL" do
+      find(:css, 'div#viewurl a').click
+      list.talks.each { |talk|
+        page.should have_content(talk.title)
+      }
     end
   end
 end
