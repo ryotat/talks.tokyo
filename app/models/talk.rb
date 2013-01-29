cc00600222b6636df61d23c9ac14236d40b217f3
Ryota Tomioka <ryota.tomi@gmail.com>
2013-01-29 14:40:14 +0900

    Fixed hostname in to_ics.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -120,7 +120,7 @@ class Talk < ActiveRecord::Base
         "DTSTART:#{start_time && start_time.getgm.to_s(:ics)}",
         "DTEND:#{end_time && end_time.getgm.to_s(:ics)}",
         "UID:TALK#{id}AT#{ActionController::Base.asset_host}",
-        "URL:#{ActionController::Base.asset_host}/talk/index/#{id}",
+        "URL:#{HOST}/talk/index/#{id}",
         "DESCRIPTION:#{abstract && abstract.to_ics}",
         "LOCATION:#{venue && venue.name && venue.name.to_ics}",
         "CONTACT:#{organiser && organiser.name && organiser.name.to_ics}",
