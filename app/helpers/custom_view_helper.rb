3c99a2032c97f984d65670f36c7ad8f6e021c683
Ryota Tomioka <ryotat@Ryota-no-MacBook-Air.local>
2013-02-18 11:39:19 +0900

    Deleted custom_view_helper.

--- app/helpers/custom_view_helper.rb
+++
@@ -1,8 +0,0 @@
-module CustomViewHelper
-  
-  def keep_url_in_sync_with_form( cview, form = '#viewform', urldiv = '#viewurl')
-    content_tag 'script', "jQuery('#{form}').talks('observe_form','#{urldiv}', '#{custom_view_path(:action => "update", :id => cview)}')".html_safe
-  end
-
-
-end
