cc30a81c075d6423dde65b0486611e2f76353f12
Ryota Tomioka <ryotat@new-host-2.fletsphone>
2013-04-29 01:14:04 +0900

    Use addthis sharing tools. Disabled tickles.

--- spec/requests/tickles_spec.rb
+++ spec/requests/tickles_spec.rb
@@ -1,42 +1,42 @@
 require 'spec_helper'
 
 describe "Tickles" do
-  subject { page }
-  describe "index", :js => true  do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:talk) { FactoryGirl.create(:talk) }
-    let(:emails) { (1..3).map { |x| "#{x}@example.com" } }
-    before do
-      sign_in user
-      visit talk_path(talk)
-      emails.each { |x| send_tickle(x) }
-    end
-    context "for administrator" do
-      let(:adminuser) { FactoryGirl.create(:user, :administrator => true) }
-      before do
-        sign_in adminuser
-        visit tickles_path
-      end
-      it { should have_content "Listing tickles" }
-      it { emails.each { |x| page.should have_content x } }
-      it { should have_no_content "4@example.com" }
-    end
-    context "for non admin" do
-      it "should not list" do
-        visit tickles_path
-        should show_404
-      end
-      it "should not show" do
-        visit tickle_path(1)
-        should show_404
-      end
-    end
-  end
-  describe "create" do
-    let(:talk) { FactoryGirl.create(:talk) }
-    before do
-      visit '/tickles/create/1?tickle[about_id]=1&tickle[about_type]=Talk&tickle[recipient_email]=a@a.jp'
-    end
-    it { should have_content "You need to be logged in to carry this out." }
-  end
+#   subject { page }
+#   describe "index", :js => true  do
+#     let(:user) { FactoryGirl.create(:user) }
+#     let(:talk) { FactoryGirl.create(:talk) }
+#     let(:emails) { (1..3).map { |x| "#{x}@example.com" } }
+#     before do
+#       sign_in user
+#       visit talk_path(talk)
+#       emails.each { |x| send_tickle(x) }
+#     end
+#     context "for administrator" do
+#       let(:adminuser) { FactoryGirl.create(:user, :administrator => true) }
+#       before do
+#         sign_in adminuser
+#         visit tickles_path
+#       end
+#       it { should have_content "Listing tickles" }
+#       it { emails.each { |x| page.should have_content x } }
+#       it { should have_no_content "4@example.com" }
+#     end
+#     context "for non admin" do
+#       it "should not list" do
+#         visit tickles_path
+#         should show_404
+#       end
+#       it "should not show" do
+#         visit tickle_path(1)
+#         should show_404
+#       end
+#     end
+#   end
+#   describe "create" do
+#     let(:talk) { FactoryGirl.create(:talk) }
+#     before do
+#       visit '/tickles/create/1?tickle[about_id]=1&tickle[about_type]=Talk&tickle[recipient_email]=a@a.jp'
+#     end
+#     it { should have_content "You need to be logged in to carry this out." }
+#   end
 end
