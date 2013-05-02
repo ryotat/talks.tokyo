7ae5251e2b3fcde45fd54c5928f704196de2992b
Ryota Tomioka <ryotat@Ryota-no-MacBook-Air.local>
2013-05-02 16:24:06 +0900

    Clean up.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -5,30 +5,37 @@ describe "Home",:js => true  do
   let(:talk1) { FactoryGirl.create(:talk, :start_time => Time.now) }
   let(:talk2) { FactoryGirl.create(:talk, :start_time => Time.now+6.days) }
   let(:talk3) { FactoryGirl.create(:talk, :start_time => Time.now+1.week) }
+  subject { page }
   before do
-    visit talk_path(:id => talk1.id)
-    visit talk_path(:id => talk2.id)
-    visit talk_path(:id => talk3.id)
+    visit talk_path(talk1)
+    visit talk_path(talk2)
+    visit talk_path(talk3)
   end
-  it "should show today's talks" do
-    visit home_path
-    click_link "Today"
-    page.should have_link(talk1.title)
-    page.should have_no_link(talk2.title)
-    page.should have_no_link(talk3.title)
+  context "today's talks" do
+    before do
+      visit home_path
+      click_link "Today"
+    end
+    it { should have_link(talk1.title) }
+    it { should have_no_link(talk2.title) }
+    it { should have_no_link(talk3.title) }
   end
-  it "should show this week's talks" do
-    visit home_path
-    click_link "This week"
-    page.should have_link(talk1.title)
-    page.should have_link(talk2.title)
-    page.should have_no_link(talk3.title)
+  context "this week's talks" do
+    before do
+      visit home_path
+      click_link "This week"
+    end
+    it { should have_link(talk1.title) }
+    it { should have_link(talk2.title) }
+    it { should have_no_link(talk3.title) }
   end
-  it "should show all talks" do
-    visit home_path
-    click_link "All"
-    page.should have_link(talk1.title)
-    page.should have_link(talk2.title)
-    page.should have_link(talk3.title)
+  context "all talks" do
+    before do
+      visit home_path
+      click_link "All"
+    end
+    it { should have_link(talk1.title) }
+    it { should have_link(talk2.title) }
+    it { should have_link(talk3.title) }
   end
 end
