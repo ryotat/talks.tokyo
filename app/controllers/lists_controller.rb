9ac7f5b7eda759acce78f0030284563b90557e41
Ryota Tomioka <ryotat@Ryota-no-MacBook-Air.local>
2013-07-02 11:40:42 +0900

    Allow ordinary users to see the list details.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
   
   before_filter :ensure_user_is_logged_in, :except =>  %w{ index }
   before_filter :find_list, :except => %w{ new create choose }
-  before_filter :check_can_edit_list, :except => %w{ index new create choose }
+  before_filter :check_can_edit_list, :except => %w{ index new create choose details }
   
   def new
     @list = List.new
