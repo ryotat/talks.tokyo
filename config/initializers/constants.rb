5352102fe1dc343041c51c6218965af6ca16a026
Ryota Tomioka <ryotat@Ryota-no-MacBook-Air.local>
2013-07-09 15:57:55 +0900

    Added keywords and description. Modified description at /users/new.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 SITE_NAME = "Talks.Tokyo"
-SITE_LOGO = "logo.png"
-SITE_LOGO_LARGE = "logo.png"
 HOST_INSTITUTE = "The University of Tokyo"
-HOST_INSTITUTE_URL = "http://www.u-tokyo.ac.jp"
-HOST_INSTITUTE_LOGO = "todai.gif"
+KEYWORDS = "scientific talks, seminars, セミナー, 勉強会, 講演会, 科学, コミュニケーション"
+DESCRIPTION = "Talks.Tokyo は科学に関わる内容のセミナーや講演会の運営、告知、共有を簡単にし、科学者同士の交流を活発にするとともに、広い社会とアカデミアをつなげることを目指しています。"
+HOST = "localhost:3000"
 WEBMASTER = "webmaster@localhost"
 SEARCH_EXAMPLES = "e.g. Surfaces and strings, Darwin lectures, Thomas Young, meerkat, Lord Adonis"
 ABST_TRUNCATION_FACTOR = {'en' => 1.0, 'ja' => 0.5}
